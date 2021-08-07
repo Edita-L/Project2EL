26d68cb2d9e0cff41f10be900fb29d0f27d89a77
Edita-L <edita.lees@gmail.com>
2021-08-07 14:43:15 +0000

    test different methods of adding questions/answers functions on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,90 @@
-var translations = [
 
-  {
-    "id": 1,
-    "question": "How to say CAT in Spanish?",
-    "original": ["Gato", "Cato", "Fato"],
-    "correct": "Gato",
-    "score": 0
-  },
-  {
-    "id": 2,
-    "question": "How to say HOUSE in Spanish?",
-    "original": ["Casa", "Vila", "Doma"],
-    "correct": "Casa",
-    "score": 0
-  },
-  {
-    "id": 3,
-    "question": "How to say POTATO in Spanish?",
-    "original": ["Patita", "Petata", "Patata"],
-    "correct": "Patata",
-    "score": 0
+function submitQuiz() {
+  console.log('submitted');
+
+// get each answer score
+  function answerScore (qName) {
+    var radiosNo = document.getElementsByName(qName);
+
+    for (var i = 0, length = radiosNo.length; i < length; i++) {
+         if (radiosNo[i].checked) {
+    // do something with radiosNo
+        var answerValue = Number(radiosNo[i].value);
+      }
+    }
+    // change NaNs to zero
+    if (isNaN(answerValue)) {
+      answerValue = 0;
+    }
+    return answerValue;
   }
-];
 
+// calc score with answerScore function
+  var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4')+ answerScore('q5'));
+  console.log("CalcScore: " + calcScore); // it works!
 
-// 1. load when page loads, gets question 0
-window.addEventListener("load", function () {
+// function to return correct answer string
+  function correctAnswer (correctStringNo, qNumber) {
+    console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
+    return ("The correct answer for question #" + qNumber + ": &nbsp;<strong>" +
+      (document.getElementById(correctStringNo).innerHTML) + "</strong>");
+    }
 
-  var question = document.getElementById("question1");
-  question.innerHTML = translations[0]["question"];
+// print correct answers only if wrong (calls correctAnswer function)
+  if (answerScore('q1') === 0) {
+    document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
+  }
+  if (answerScore('q2') === 0) {
+    document.getElementById('correctAnswer2').innerHTML = correctAnswer('correctString2', 2);
+  }
+  if (answerScore('q3') === 0) {
+    document.getElementById('correctAnswer3').innerHTML = correctAnswer('correctString3', 3);
+  }
+  if (answerScore('q4') === 0) {
+    document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctString4', 4);
+  }
+  if (answerScore('q5') === 0) {
+    document.getElementById('correctAnswer5').innerHTML = correctAnswer('correctString5', 5);
+  }
 
-  var answer11 = document.getElementById('qa1');
-  answer11.innerHTML = translations[0]["original"][0];
+// calculate "possible score" integer
+  var questionCountArray = document.getElementsByClassName('question');
 
-  var answer12 = document.getElementById('qa2');
-  answer12.innerHTML = translations[0]["original"][1];
+  var questionCounter = 0;
+  for (var i = 0, length = questionCountArray.length; i < length; i++) {
+    questionCounter++;
+  }
 
+// show score as "score/possible score"
+  var showScore = "Your Score: " + calcScore +"/" + questionCounter;
+// if 4/4, "perfect score!"
+  if (calcScore === questionCounter) {
+    showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>"
+  };
+  document.getElementById('userScore').innerHTML = showScore;
+}
 
-  var answer13 = document.getElementById('qa3');
-  answer13.innerHTML = translations[0]["original"][2];
+$(document).ready(function() {
+
+$('#submitButton').click(function() {
+  $(this).addClass('hide');
+});
 
 });
+
+// 1. load when page loads, gets question 0
+/*window.addEventListener("load", function () {
+
+    /*var question = document.getElementById("question1");
+     question.innerHTML = translations[0]["question"];
+
+    var answer11 = document.getElementById('qa1');
+     answer11.innerText = translations[0]["original"][0];
+
+     var answer12 = document.getElementById('qa2');
+     answer12.innerText = translations[0]["original"][1];
+
+
+     var answer13 = document.getElementById('qa3');
+     answer13.innerText = translations[0]["original"][2];*/
+    
